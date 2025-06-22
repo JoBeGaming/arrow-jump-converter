22323e1fb1aa20c76e9aa00dccefaea8ad031879
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2025-06-22 22:27:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ import re
 import collections.abc
 
 
-from main import parse
+from main import parse, TARGET_PLACEHOLDER
 
 
-def replace_in_order(string: str, replacements: collections.abc.Iterable[str], patter: str = ...) -> str:
+def replace_ordered(string: str, replacements: collections.abc.Iterable[str], patter: str = TARGET_PLACEHOLDER) -> str:
     repl_iter = iter(replacements)
     
     def repl_func(match):
@@ -23,7 +23,22 @@ def replace_in_order(string: str, replacements: collections.abc.Iterable[str], p
 
 
 
-TestStr1: str = """
+TestStr1: str = f"""
+...
 """
 
-TestRes1: str = replace_TestStr1
+TestRes1: str = replace_ordered(TestStr1, [...])
+
+
+TestStr2: str = f"""
+...
+"""
+
+TestRes2: str = replace_ordered(TestRes2, [...])
+
+
+
+if __name__ == "__main__":
+    assert TestStr1 == TestRes1
+    assert TestStr2 == TestRes2
+    ...
