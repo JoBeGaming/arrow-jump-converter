2adcaa8c3d5113ea59c2202f1f3ec21df55ed005
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2025-06-22 22:20:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,27 @@
 # (c) JoBe, 2025
 
 
+import re
+import collections.abc
+
+
 from main import parse
 
 
-...
+def replace_in_order(string: str, replacements: collections.abc.Iterable[str], patter: str = ...) -> str:
+    repl_iter = iter(replacements)
+    
+    def repl_func(match):
+        try:
+            return next(repl_iter)
+        except StopIteration:
+            return match.group(0)
+    
+    return re.sub(pattern, repl_func, s)
+
+
+
+TestStr1: str = """
+"""
+
+TestRes1: str = replace_TestStr1
